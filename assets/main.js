db59aca6a0fae4b7ca9565022add5a524fa5990a
Ian A <iastesana@gmail.com>
2020-07-28 17:31:13 -0700

    Made the color changing by time theme. Need to message the code to make it look pretty.

--- assets/main.js
+++ assets/main.js
@@ -4,10 +4,20 @@ $(".saveBtn").on("click", function(){
     console.log(userData,timeBlock)
     localStorage.setItem(timeBlock, userData)
 })
+var currentTime = moment().hour
     for (let i = 8; i <= 16; i++) {
         const element = localStorage.getItem(i);
         $(`#${i}-input`).val(element)
+        if (currentTime > i ){
+            $(`#${i}`).addClass("bg-danger")
+        } else if (currentTime == i ){
+            $(`#${i}`).addClass("bg-warning")
+        } else {
+            $(`#${i}`).addClass("bg-success")
+        }
     }
 var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#todaysDate").append(todaysDate)
+
+
 
