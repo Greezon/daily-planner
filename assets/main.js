44b125dd269cb490765c8b11d619fe8b4cc32b1b
Ian A <iastesana@gmail.com>
2020-07-31 15:46:24 -0700

    Styled the containers, and fixed some bugs. Added a readme png. On the final stretch to submit.

--- assets/main.js
+++ assets/main.js
@@ -4,20 +4,26 @@ $(".saveBtn").on("click", function(){
     console.log(userData,timeBlock)
     localStorage.setItem(timeBlock, userData)
 })
-var currentTime = moment().hour
+var currentTime = moment().hour()
     for (let i = 8; i <= 16; i++) {
         const element = localStorage.getItem(i);
         $(`#${i}-input`).val(element)
         if (currentTime > i ){
-            $(`#${i}`).addClass("bg-danger")
+            $(`#${i}`).addClass("bg-info text-light")
+            console.log("Have a good morning!")
         } else if (currentTime == i ){
             $(`#${i}`).addClass("bg-warning")
+            console.log("Hey")
         } else {
             $(`#${i}`).addClass("bg-success")
+            console.log("Have a night")
         }
+
     }
 var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#todaysDate").append(todaysDate)
+
+
 
 
 
